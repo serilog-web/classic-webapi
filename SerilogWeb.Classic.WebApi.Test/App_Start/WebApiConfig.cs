945dcafaf57ed2d983f6c28a3aaf523b5b57f110
Chui Tey <chui.tey@readify.net>
2016-01-15 00:18:34 +1000

    Removed bundleconfig

--- SerilogWeb.Classic.WebApi.Test/App_Start/WebApiConfig.cs
+++ SerilogWeb.Classic.WebApi.Test/App_Start/WebApiConfig.cs
@@ -7,11 +7,6 @@ namespace SerilogWeb.Classic.WebApi.Test
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-            // Configure Web API to use only bearer token authentication.
-            config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
             // Web API routes
             config.MapHttpAttributeRoutes();
 
--- SerilogWeb.Classic.WebApi.Test/Global.asax.cs
+++ SerilogWeb.Classic.WebApi.Test/Global.asax.cs
@@ -17,7 +17,6 @@ namespace SerilogWeb.Classic.WebApi.Test
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
