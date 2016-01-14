76c1f98698582358faa74219e0c8562dc02e0660
Chui Tey <chui.tey@readify.net>
2016-01-15 00:13:01 +1000

    Implementation of solution

---
+++ SerilogWeb.Classic.WebApi/PreApplicationStartModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+
+namespace SerilogWeb.Classic.WebApi
+{
+    class PreApplicationStartModule
+    {
+        public static void Register()
+        {
+            GlobalConfiguration.Configuration.Services.Add(typeof(IExceptionLogger),
+                new WebApiExceptionLogger());
+        }
+    }
+}
---
+++ SerilogWeb.Classic.WebApi/WebApiExceptionLogger.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Http.ExceptionHandling;
+
+namespace SerilogWeb.Classic.WebApi
+{
+    public class WebApiExceptionLogger : ExceptionLogger
+    {
+        
+        public override void Log(ExceptionLoggerContext context)
+        {
+            HttpContext.Current.AddError(context.Exception);
+        }
+
+    }
+}
