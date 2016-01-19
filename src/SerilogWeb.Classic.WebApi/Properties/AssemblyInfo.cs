1cc194dbb84fb7a1740b21165df18086e279d77c
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2016-01-19 21:38:27 +1000

    A few tweaks to match the SerilogWeb.Classic project structure, so we can keep the build consistent (#1)

---
+++ src/SerilogWeb.Classic.WebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Web;
+using SerilogWeb.Classic.WebApi;
+
+[assembly: AssemblyTitle("SerilogWeb.Classic.WebApi")]
+[assembly: PreApplicationStartMethod(typeof(PreApplicationStartModule), "Register")]
